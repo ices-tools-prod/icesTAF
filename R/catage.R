a9199bd0f601587fc62e5aef35d1aaf690befe12
Arni Magnusson <arnima@hafro.is>
2017-05-04 16:08:16 +0200

    Add catage* data objects

---
+++ R/catage.R
@@ -0,0 +1,27 @@
+#' @docType data
+#'
+#' @name catage
+#'
+#' @aliases catage.long catage.taf catage.xtab
+#'
+#' @title Catch at Age Examples
+#'
+#' @description
+#' Small catch-at-age table to describe three formats to store year-age values
+#' in a data frame.
+#'
+#' @format
+#' All three data frames contain the same data in alternative formats:
+#' \tabular{lll}{
+#'   \code{catage.long} \tab long format     \tab (three columns)\cr
+#'   \code{catage.taf}  \tab TAF format      \tab (year column)\cr
+#'   \code{catage.xtab} \tab crosstab format \tab (year as row names)
+#' }
+#'
+#' @references
+#' ICES. 2016. Report of the working group on the assessment of demersal stocks
+#' in the North Sea and Skagerrak (WGNSSK).
+#' \href{http://ices.dk/sites/pub/Publication\%20Reports/Expert\%20Group\%20Report/acom/2016/WGNSSK/01\%20WGNSSK\%20report\%202016.pdf}{\cite{ICES
+#' CM 2016/ACOM:14}}, p. 656.
+
+NA
