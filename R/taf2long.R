92dfbad2d85d2ec219b5aa668474442f2c2a8dc1
Arni Magnusson <arnima@hafro.is>
2017-05-04 17:37:17 +0200

    Simplify taf2long()

--- R/taf2long.R
+++ R/taf2long.R
@@ -4,16 +4,14 @@
 #'
 #' @param x a data frame in TAF format.
 #' @param names a vector of three column names for the resulting data frame.
-#' @param year whether the input table has year in the first column, instead of
-#' row names.
 #'
 #' @return
 #' A data frame with three columns.
 #'
 #' @note
 #' TAF stores tables as data frames with a year column, as seen in stock
-#' assessment reports. The long format is practical for analysis and producing
-#' plots.
+#' assessment reports. The long format is more convenient for analysis and
+#' producing plots.
 #'
 #' @seealso
 #' \code{\link{flr2taf}} converts FLR tables to TAF format.
@@ -21,24 +19,17 @@
 #' \code{\link{icesTAF-package}} gives an overview of the package.
 #'
 #' @examples
-#' fish <- matrix(round(runif(30),3), 5)
-#' fish <- data.frame(Year=2001:2005, fish, check.names=FALSE)
-#' taf2long(fish)
-#'
-#' alt <- fish[-1]
-#' row.names(alt) <- fish$Year
-#' taf2long(alt, names=c("Year","Age","Weight"), year=FALSE)
+#' taf2long(catage.taf, names=c("Year","Age","Catch"))
 #'
 #' @export
 
-taf2long <- function(x, names=c("Year","Age","Value"), year=TRUE)
+taf2long <- function(x, names=c("Year","Age","Value"))
 {
-  if(year)
-  {
-    row.names(x) <- x[[1]]
-    x <- x[-1]
-  }
+  row.names(x) <- x[[1]]
+  x <- x[-1]
   y <- as.data.frame(as.table(as.matrix(x)))
+  y[[1]] <- as.integer(as.character(y[[1]]))
+  y[[2]] <- as.integer(as.character(y[[2]]))
   names(y) <- names
   y
 }
