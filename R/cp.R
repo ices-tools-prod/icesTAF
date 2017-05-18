2abbc948584e919d3cf5fde0b3bbeeef3f84e563
Arni Magnusson <arnima@hafro.is>
2017-05-18 14:40:44 +0200

    New function cp() to copy file

---
+++ R/cp.R
@@ -0,0 +1,32 @@
+#' Copy File
+#'
+#' Copy file, overwriting existing file if necessary, and returning the result
+#' invisibly.
+#'
+#' @param from source file name.
+#' @param to destination file name.
+#'
+#' @return
+#' \code{TRUE} for success, \code{FALSE} for failure, invisibly.
+#'
+#' @note
+#' Shorthand for \code{invisible(file.copy(..., overwrite=TRUE))}.
+#'
+#' @seealso
+#' \code{\link{file.copy}}.
+#'
+#' \code{\link{icesTAF-package}} gives an overview of the package.
+#'
+#' @examples
+#' \dontrun{
+#' write(pi, "A.txt")
+#' cp("A.txt", "B.txt")
+#' file.remove("A.txt", "B.txt")
+#' }
+#'
+#' @export
+
+cp <- function(from, to)
+{
+  invisible(file.copy(from, to, overwrite=TRUE))
+}
