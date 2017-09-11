742223aae144f3529fee7957045949094e959d06
Arni Magnusson <arnima@hafro.is>
2017-09-11 13:02:53 +0200

    sourceAtoZ does not ignore all.R - no longer part of standard workflow

--- R/sourceAtoZ.R
+++ R/sourceAtoZ.R
@@ -1,8 +1,8 @@
 #' Run TAF Scripts in Alphabetical Order
 #'
-#' Run all TAF scripts in a directory, except \file{all.R}. Scripts are run
-#' sequentially, in alphabetical order. Optionally, start by cleaning old output
-#' directories and emptying the workspace before running each script.
+#' Run all TAF scripts in a directory, in alphabetical order. Optionally, start
+#' by cleaning old output directories and emptying the workspace before running
+#' each script.
 #'
 #' @param path directory containing TAF scripts.
 #' @param rm whether to remove all objects from the global environment before
@@ -19,13 +19,6 @@
 #'
 #' @return
 #' Logical vector, indicating which scripts ran without errors.
-#'
-#' @note
-#' The \code{sourceAtoZ} function ignores \file{all.R} if such a script exists.
-#' The reason for this is that many TAF assessments have a script called
-#' \file{all.R} that runs all (or selected) scripts in alphabetical order.
-#' Running \file{all.R} and then running all other scripts would effectively
-#' repeat the entire assessment procedure twice.
 #'
 #' @seealso
 #' \code{\link{sourceTAF}} runs a TAF script.
@@ -49,7 +42,6 @@ sourceAtoZ <- function(path=".", rm=TRUE, clean=TRUE, quiet=FALSE)
     clean(path)
 
   scripts <- dir(path, pattern="\\.[Rr]$")
-  scripts <- grep("^all\\.[Rr]", scripts, invert=TRUE, value=TRUE)
 
   ok <- sapply(scripts, function(x) sourceTAF(x, rm=rm, quiet=quiet))
   if(length(ok) == 0)
