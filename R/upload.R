1ed4254d9b2d8e20168a697bef40f3492ddf4813
Arni Magnusson <arnima@hafro.is>
2017-09-13 15:17:00 +0200

    Upload raw data or model executable

--- R/upload.R
+++ R/upload.R
@@ -1,43 +1,51 @@
 #' Upload File to TAF Database
 #'
-#' Upload a file containing raw data to the TAF database.
+#' Upload a file to the TAF database, either containing \emph{raw} data or
+#' \emph{model} executable.
 #'
-#' @param analysis name of TAF analysis, e.g. \code{2015_rjm-347d}.
-#' @param file filename, e.g. \code{surveys_all.csv}.
+#' @param analysis name of TAF analysis.
+#' @param type file type, either \code{"raw"} or \code{"model"}.
+#' @param file filename.
 #'
-#' @note
-#' The file will be stored in the TAF database and made available in the
-#' \file{raw} directory on the file server, e.g.
-#' \url{http://taf.ices.local/taf/fs/2015_rjm-347d/raw/catch.csv}.
-#'
-#' The term \emph{raw data} refers to the initial data step to be archived.
-#' These archived data files are the basis of all subsequent analysis, and
-#' guarantee that the TAF analysis can be rerun later and will reproduce the
-#' original results, even if data values in the underlying databases (outside of
-#' TAF) may have changed after the original analysis was submitted.
+#' @section Raw data:
+#' Raw data refers to the initial data step to be archived. These files are the
+#' basis of all subsequent analysis, and guarantee that the TAF analysis can be
+#' rerun later and will reproduce the original results, even if data in the
+#' underlying databases (outside of TAF) may have changed after the original
+#' analysis was submitted.
 #'
 #' One objective of TAF is to document the data preparation. Therefore, the raw
 #' data files should represent the original data before the main preprocessing
 #' and data aggregation takes place. For example, the raw data could be the
 #' result of an SQL database query, selecting one species of interest and all
 #' relevant data columns for the analysis.
+
+#' @section Model executables:
+#' When using a model that is not in the ICES Toolbox of stock assessment
+#' models, the model executables are uploaded to the TAF database before the
+#' model is run.
+#'
+#' To make the analysis reproducible across platforms, two executables should be
+#' uploaded, one for Linux and one for Windows. The source code is not uploaded
+#' in the same way, but is version-controlled with the rest of the TAF analysis.
 #'
 #' @seealso
 #' \code{\link{icesTAF-package}} gives an overview of the package.
 #'
 #' @examples
 #' \dontrun{
-#' upload("2015_rjm-347d", "surveys_all.csv")
+#' upload("2015_rjm-347d", "raw", "surveys_all.csv")
 #' }
 #'
 #' @importFrom httr POST upload_file
 #'
 #' @export
 
-upload <- function(analysis, file)
+upload <- function(analysis, type, file)
 {
+  type <- match.arg(type, c("raw", "model"))
   file <- upload_file(file)
-  url <- sprintf("http://taf.ices.local/taf/fs/%s/raw/%s",
-                 analysis, basename(file$path))
+  url <- sprintf("http://taf.ices.local/taf/fs/%s/%s/%s",
+                 analysis, type, basename(file$path))
   POST(url, body=file)
 }
