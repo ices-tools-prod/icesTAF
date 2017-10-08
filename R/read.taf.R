81e69fa3fa91e0dada984db3f968dbd1727cf988
Arni Magnusson <arnima@hafro.is>
2017-10-08 08:37:53 +0200

    Set UTF-8 as default encoding in read.taf() and write.taf()

--- R/read.taf.R
+++ R/read.taf.R
@@ -3,11 +3,18 @@
 #' Read a TAF table from a file into a data frame.
 #'
 #' @param file a filename.
+#' @param check.names whether to enforce regular column names, e.g. convert
+#'        column name \samp{3} to \samp{X3}.
+#' @param fileEncoding character encoding of input file.
+#' @param \dots passed to \code{read.csv}.
 #'
 #' @return
 #' A data frame in TAF format.
 #'
 #' @seealso
+#' \code{\link{read.csv}} is the underlying function used to read a table from a
+#' file.
+#'
 #' \code{\link{write.taf}} writes a TAF table to a file.
 #'
 #' \code{\link{icesTAF-package}} gives an overview of the package.
@@ -24,7 +31,7 @@
 #'
 #' @export
 
-read.taf <- function(file)
+read.taf <- function(file, check.names=FALSE, fileEncoding="UTF-8-BOM", ...)
 {
-  read.csv(file, check.names=FALSE)
+  read.csv(file, check.names=check.names, fileEncoding=fileEncoding, ...)
 }
