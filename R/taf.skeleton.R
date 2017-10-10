df0ab5c1ddfa29a446412606c42a772f8a2fc624
colin millar <colinpmillar@gmail.com>
2017-10-10 14:07:28 +0200

    taf.skeleton revisions

--- R/taf.skeleton.R
+++ R/taf.skeleton.R
@@ -6,19 +6,61 @@
 #' file describing further steps to create a functioning TAF analysis.
 #'
 #'
-#' @param name character string: the package name and directory name for your package.
-#' @param list character vector naming the R objects to put in the package. Usually, at most one of list, environment, or code_files will be supplied. See ‘Details’.
-#' @param environment	an environment where objects are looked for. See ‘Details’.
-#' @param path path to put the package directory in.
+#' @param name character string: the name and directory name for your analysis,
+#'             if NULL then it effectively assumes the parent directory name as the
+#'             analysis name.
+#' @param path path to put the analysis directory in.
 #' @param force	If FALSE will not overwrite an existing directory.
-#' @param code_files a character vector with the paths to R code files to build the package around. See ‘Details’.
-#' @param encoding optionally a character string with an encoding for an optional Encoding: line in ‘DESCRIPTION’ when non-ASCII characters will be used; typically one of "latin1", "latin2", or "UTF-8"; see the WRE manual.
 #'
 #' @export
 
-taf.skeleton <- function (name = "aTAFanalysis", list = character(), environment = .GlobalEnv,
-    path = ".", force = FALSE, code_files = character(), encoding = "UTF-8-DOM")
+taf.skeleton <- function (name = "aTAFanalysis", path = ".",
+                          force = FALSE, encoding = "UTF-8")
 {
+  owd <- setwd(path)
+  on.exit(setwd(owd))
+  # create analysis directory
+  if (!is.null(name)) {
+    mkdir(name)
+    setwd(name)
+  }
 
+  # create directory structure
+  mkdir("_raw")
+  mkdir("_config")
+
+  # add in basic R files with header info
+  header_template <- "## %s\n\n## Before:\n## After:"
+  headers <- list(`_raw` = "Upload raw data to TAF database",
+                  data = "Preprocess data, write TAF data tables",
+                  input = "Convert data to model format, write model input files",
+                  model = "Run analysis, write model results",
+                  output = "Extract model results of interest, write TAF output tables",
+                  report = "Plot data and results")
+
+  safe.cat <- function(..., file, force, sep = " ", fill = FALSE, labels = NULL, append = FALSE) {
+    if (file.exists(file) && force) {
+      unlink(file)
+    }
+    cat(..., file = file, sep = sep, fill = fill, labels = labels, append = append)
+  }
+
+  for (section in names(headers)) {
+    if (section == "_raw") {
+      safe.cat(file = file.path(section, "upload.R"),
+          sprintf(header_template, headers[[section]]),
+          force = force)
+    } else {
+      safe.cat(file = paste0(section, ".R"),
+          sprintf(header_template, headers[[section]]),
+          force = force)
+    }
+  }
+
+  # check names of list entries:
+  #   - should be _raw, _config, scripts?
+
+  # return directory created analysis
+  return(path.expand(path))
 }
 
