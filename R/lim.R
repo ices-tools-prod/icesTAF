64fca312f33723262803ac5b755d9bb956cd5cf0
Arni Magnusson <arnima@hafro.is>
2017-10-16 15:20:56 +0200

    Change 'mult' 1.05 -> 1.1

--- R/lim.R
+++ R/lim.R
@@ -1,10 +1,10 @@
 #' Axis Limits
 #'
 #' Compute axis limits. The lower limit is 0 and the upper limit is determined
-#' by the highest data value, multiplied with a scalar.
+#' by the highest data value, times a multiplier.
 #'
 #' @param x a vector of data values.
-#' @param mult a scalar to multiply with the highest data value.
+#' @param mult a number to multiply with the highest data value.
 #'
 #' @return
 #' A vector of length two, which can be used as axis limits.
@@ -17,7 +17,7 @@
 #'
 #' @export
 
-lim <- function(x, mult=1.05)
+lim <- function(x, mult=1.1)
 {
   c(0, mult*max(x,na.rm=TRUE))
 }
