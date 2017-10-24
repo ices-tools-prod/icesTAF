b2d138f986c22f2931ab22f52dee399d58476e07
Arni Magnusson <arnima@hafro.is>
2017-10-24 19:08:57 +0200

    Add argument 'column' to tt()

--- R/tt.R
+++ R/tt.R
@@ -3,6 +3,10 @@
 #' Convert a table from TAF format to transposed crosstab format.
 #'
 #' @param x a data frame in TAF format.
+#' @param column a logical indicating whether the group names should be stored
+#'        in a column called \samp{Age} instead of in row names. Alternatively,
+#'        \code{column} can be a string supplying another name for that first
+#'        column.
 #'
 #' @return
 #' A data frame with years as column names.
@@ -22,10 +26,21 @@
 #' @examples
 #' taf2xtab(catage.taf)
 #' tt(catage.taf)
+#' tt(catage.taf, TRUE)
+#' tt(catage.taf, "Custom")
 #'
 #' @export
 
-tt <- function(x)
+tt <- function(x, column=FALSE)
 {
-  as.data.frame(t(taf2xtab(x)))
+  y <- as.data.frame(t(taf2xtab(x)))
+  if(isTRUE(column))
+    column <- "Age"
+  if(is.character(column))
+  {
+    y <- data.frame(row.names(y), y, check.names=FALSE)
+    names(y)[1] <- column
+    row.names(y) <- NULL
+  }
+  y
 }
