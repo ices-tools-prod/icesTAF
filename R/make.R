d32c4281fab8ce6515ca3a946d6b5ca248065bfd
Arni Magnusson <arnima@hafro.is>
2017-12-28 16:49:14 +0000

    Document how prereq can be scripts, target can be dir, link to GNU Make

--- R/make.R
+++ R/make.R
@@ -1,10 +1,12 @@
 #' Run R Script If Needed
 #'
-#' Run an R script if underlying data files have changed, otherwise do nothing.
+#' Run an R script if underlying files have changed, otherwise do nothing.
 #'
 #' @param recipe script filename.
-#' @param prereq one or more underlying data files, required by the script.
-#' @param target one output file, produced by the script.
+#' @param prereq one or more underlying files, required by the script. For
+#'        example, data files and/or scripts.
+#' @param target one output file, produced by the script. This can also be a
+#'        directory name.
 #' @param include whether to automatically include the script as a prerequisite
 #'        file.
 #' @param engine function to source the script.
@@ -13,6 +15,17 @@
 #' @param \dots passed to \code{engine}.
 #'
 #' @return \code{TRUE} or \code{FALSE}, indicating whether the script was run.
+#'
+#' @note
+#' This function provides functionality similar to makefile rules, to determine
+#' whether a script should be (re)run or not.
+#'
+#' If \code{target} is newer than any \code{prereq}, then the script is run.
+#'
+#' @references
+#' Stallman, R. M. \emph{et al.} An introduction to makefiles. Chapter 2 in the
+#' \emph{\href{https://www.gnu.org/software/make/manual/make.pdf}{GNU Make
+#' manual}}.
 #'
 #' @seealso
 #' \code{\link{source}} runs any R script, \code{\link{sourceTAF}} is more
