34ea951484f17c06f26ca58cfa0d53accd9f7995
Arni Magnusson <arnima@hafro.is>
2017-12-29 15:42:03 +0000

    When makeTAF("data.R") is run, include data_foo.R and data_bar.R

--- R/makeTAF.R
+++ R/makeTAF.R
@@ -8,6 +8,12 @@
 #'
 #' @return \code{TRUE} or \code{FALSE}, indicating whether the script was run.
 #'
+#' @note
+#' Any underlying scripts are automatically included if they share the same
+#' filename prefix, followed by an underscore. For example, when determining
+#' whether a script \verb{data.R} should be run, this function checks whether
+#' \verb{data_foo.R} and \verb{data_bar.R} have been recently modified.
+#'
 #' @seealso
 #' \code{\link{source}} runs any R script, \code{\link{sourceTAF}} is more
 #' convenient for running a TAF script, and \code{\link{sourceAll}} runs all TAF
@@ -32,11 +38,21 @@ makeTAF <- function(script, ...)
   on.exit(setwd(owd))
   script <- basename(script)
   out <- switch(script,
-                "data.R"=make("data.R", NULL, "data", engine=sourceTAF, ...),
-                "input.R"=make("input.R", "data", "input", engine=sourceTAF, ...),
-                "model.R"=make("model.R", "input", "model", engine=sourceTAF, ...),
-                "output.R"=make("output.R", "model", "output", engine=sourceTAF, ...),
-                "report.R"=make("report.R", "output", "report", engine=sourceTAF, ...),
+                "data.R"=make("data.R",
+                              dir(pattern="^data_.*\\.R$"),
+                              "data", engine=sourceTAF, ...),
+                "input.R"=make("input.R",
+                               c("data",dir(pattern="^input_.*\\.R$")),
+                               "input", engine=sourceTAF, ...),
+                "model.R"=make("model.R",
+                               c("input",dir(pattern="^model_.*\\.R$")),
+                               "model", engine=sourceTAF, ...),
+                "output.R"=make("output.R",
+                                c("model",dir(pattern="^output_.*\\.R$")),
+                                "output", engine=sourceTAF, ...),
+                "report.R"=make("report.R",
+                                c("output",dir(pattern="^report_.*\\.R$")),
+                                "report", engine=sourceTAF, ...),
                 FALSE)
   invisible(out)
 }
