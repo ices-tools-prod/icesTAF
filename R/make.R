14defbbaad3811c8107e39f1ea9c04060335f7ca
Arni Magnusson <arnima@hafro.is>
2018-01-04 13:14:37 +0100

    Generalize, so target can be one or more output files

--- R/make.R
+++ R/make.R
@@ -5,10 +5,10 @@
 #' @param recipe script filename.
 #' @param prereq one or more underlying files, required by the script. For
 #'        example, data files and/or scripts.
-#' @param target one output file, produced by the script. This can also be a
-#'        directory name.
-#' @param include whether to automatically include the script as a prerequisite
-#'        file.
+#' @param target one or more output files, produced by the script. Directory
+#'        names can also be used.
+#' @param include whether to automatically include the script itself as a
+#'        prerequisite file.
 #' @param engine function to source the script.
 #' @param debug whether to show a diagnostic table of files and time last
 #'        modified.
@@ -20,7 +20,8 @@
 #' This function provides functionality similar to makefile rules, to determine
 #' whether a script should be (re)run or not.
 #'
-#' If \code{target} is newer than any \code{prereq}, then the script is run.
+#' If any \code{target} is missing or older than any \code{prereq}, then the
+#' script is run.
 #'
 #' @references
 #' Stallman, R. M. \emph{et al.} An introduction to makefiles. Chapter 2 in the
@@ -51,12 +52,14 @@ make <- function(recipe, prereq, target, include=TRUE, engine=source,
   if(include)
     prereq <- union(prereq, recipe)
   if(debug)
-    print(data.frame(Object=c("target",rep("prereq",length(prereq))),
+    print(data.frame(Object=c(rep("target",length(target)),
+                              rep("prereq",length(prereq))),
                      File=c(target,prereq),
                      Modified=file.mtime(c(target,prereq))))
   if(!all(file.exists(prereq)))
     stop("missing prerequisite file '", prereq[!file.exists(prereq)][1], "'")
-  if(!file.exists(target) || file.mtime(target) < max(file.mtime(prereq)))
+  if(!all(file.exists(target)) ||
+     min(file.mtime(target)) < max(file.mtime(prereq)))
   {
     engine(recipe, ...)
     out <- TRUE
