b49b42e5785e06e956c5ba937ff5307583a83bf6
Arni Magnusson <arnima@hafro.is>
2018-03-19 14:05:39 +0100

    Resurrect previous sourceTAF() approach: 'rm' instead of 'local'

--- R/sourceTAF.R
+++ R/sourceTAF.R
@@ -3,17 +3,17 @@
 #' Run a TAF script and return to the original directory.
 #'
 #' @param script script filename.
-#' @param local whether to run script in a local temporary environment, so that
-#'        objects are not created in the global workspace.
+#' @param rm whether to remove all objects from the global environment before
+#'        the script is run.
 #' @param clean whether to remove the corresponding TAF directory before running
 #'        the script.
 #' @param quiet whether to suppress messages reporting progress.
 #'
 #' @details
-#' By default, TAF scripts are run with \code{local = TRUE} to make sure that
-#' only files, not objects, are carried over between scripts. Likewise, the
-#' default \code{clean = TRUE} makes sure that the script starts by creating a
-#' new empty directory.
+#' By default, TAF scripts are run with \code{rm = TRUE} to make sure each
+#' script starts with an empty workspace. Likewise, the default
+#' \code{clean = TRUE} makes sure that the script starts by creating a new empty
+#' directory.
 #'
 #' @return
 #' \code{TRUE} or \code{FALSE}, indicating whether the script ran without
@@ -46,15 +46,18 @@
 #'
 #' @export
 
-sourceTAF <- function(script, local=TRUE, clean=TRUE, quiet=FALSE)
+sourceTAF <- function(script, rm=TRUE, clean=TRUE, quiet=FALSE)
 {
+  if(rm)
+    rm(list=ls(.GlobalEnv), pos=.GlobalEnv)
   if(clean && dir.exists(file_path_sans_ext(script)))
     unlink(file_path_sans_ext(script), recursive=TRUE)
   if(!quiet)
     msg(script, " running...")
 
-  envir <- if(local) new.env() else .GlobalEnv
-  result <- try(sys.source(script, envir=envir, chdir=TRUE))
+  owd <- setwd(dirname(script))
+  on.exit(setwd(owd))
+  result <- try(source(basename(script)))
 
   ok <- class(result) != "try-error"
   if(!quiet)
