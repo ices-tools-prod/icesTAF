6ec774922f72fb313d657d6cfbdb8a17d31518a6
Arni Magnusson <arnima@hafro.is>
2018-05-10 22:40:29 +0200

    Explicit print() to export trellis and ggplot

--- R/tafpng.R
+++ R/tafpng.R
@@ -4,19 +4,19 @@
 #' folder.
 #'
 #' @param filename image filename.
-#' @param width image width (pixels).
-#' @param height image height (pixels).
+#' @param width image width.
+#' @param height image height.
 #' @param pointsize text size.
 #' @param \dots passed to \code{png}.
 #'
 #' @details
 #' The \code{filename} can be passed without the preceding
-#' \verb{report/}, and without the \verb{.png} filename extension.
+#' \code{"report/"}, and without the \code{".png"} filename extension.
 #'
-#' Specifically, the function prepends \verb{"report/"} to the filename if (1)
-#' the filename does not contain a \verb{"/"} separator, (2) the working
-#' directory is not \verb{"report"}, and (3) the directory \verb{"report"}
-#' exists. The function appends \verb{".png"} to the filename if it does not
+#' Specifically, the function prepends \code{"report/"} to the filename if (1)
+#' the filename does not contain a \code{"/"} separator, (2) the working
+#' directory is not \verb{report}, and (3) the directory \verb{report} exists.
+#' The function also appends \verb{".png"} to the filename if it does not
 #' already have that filename extension.
 #'
 #' This automatic filename manipulation can be bypassed by using the \code{png}
@@ -27,6 +27,9 @@
 #' \preformatted{png("report/myplot.png", width=800, height=600, pointsize=22)}
 #' to
 #' \preformatted{tafpng("myplot")}
+#' To successfully export \verb{trellis} and \verb{ggplot} objects to image
+#' files in scripts, these objects should be explicitly printed (see examples
+#' below).
 #'
 #' @seealso
 #' \code{\link{png}} is the underlying function used to open a PNG graphics
@@ -38,6 +41,16 @@
 #' \dontrun{
 #' tafpng("myplot")
 #' plot(1)
+#' dev.off()
+#'
+#' library(lattice)
+#' tafpng("mytrellis")
+#' print(xyplot(1~1))
+#' dev.off()
+#'
+#' library(ggplot2)
+#' tafpng("myggplot")
+#' print(qplot(1))
 #' dev.off()
 #' }
 #'
