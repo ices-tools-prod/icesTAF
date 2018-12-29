970b34c0e1ca7ffdd9ff322f139535de45c80cd0
Arni Magnusson <arnima@hafro.is>
2018-12-29 13:04:52 +0000

    Update taf.colors

--- R/taf.colors.R
+++ R/taf.colors.R
@@ -20,5 +20,13 @@
 #'
 #' @examples
 #' taf.green
+#'
 #' barplot(5:1, col=c(taf.green, taf.orange, taf.blue, taf.dark, taf.light))
+#'
+#' barplot(6:1, col=c(taf.green, taf.orange, taf.blue,
+#'                    taf.dark, taf.light, "white"))
+#'
+#' barplot(7:1, col=c("black", taf.dark, taf.light,
+#'                    taf.green, taf.orange, taf.blue, "white"))
+
 NA
