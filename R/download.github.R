46edc2648a282e808fb1275eaa6d98d9f28a6a6e
Arni Magnusson <arnima@hafro.is>
2019-08-02 12:24:18 +0200

    download.github() handles repo/subdir

--- R/download.github.R
+++ R/download.github.R
@@ -1,4 +1,4 @@
-# Download GitHub Repository
+#' Download GitHub Repository
 #'
 #' Download a repository from GitHub in \file{tar.gz} format.
 #'
@@ -23,18 +23,38 @@
 #' }
 #'
 #' @importFrom remotes parse_repo_spec
+#' @importFrom utils tar untar
 #'
 #' @export
 
 download.github <- function(repo, dir=".")
 {
-  mkdir(dir)
+  mkdir(dir)  # bootstrap/software
+  owd <- setwd(dir); on.exit(setwd(owd))
+
+  ## 1  Download
   spec <- parse_repo_spec(repo)
   sha <- get_remote_sha(spec$username, spec$repo, spec$ref)  # branch -> sha
-  spec$ref <- substring(sha, 1, 7)
+  sha <- substring(sha, 1, 7)
   url <- paste0("https://api.github.com/repos/",
                 spec$username, "/", spec$repo, "/tarball/", spec$ref)
-  targz <- paste0(spec$repo, "_", spec$ref, ".tar.gz")
-  if(!file.exists(file.path(dir, targz)))
-    suppressWarnings(download(url, destfile=file.path(dir, targz)))
+  targz <- paste0(spec$repo, "_", sha, ".tar.gz")  # repo_sha.tar.gz
+  if(!file.exists(targz))
+    suppressWarnings(download(url, destfile=targz))
+
+  ## 2  Handle subdir
+  if(spec$subdir != "")
+  {
+    repdir <- basename(untar(targz, list=TRUE)[1])  # top directory inside targz
+    subdir <- spec$subdir
+    untar(targz, file.path(repdir, subdir))         # extract subdir
+    file.remove(targz)
+    ## Move bootstrap/software/repdir/subdir to bootstrap/software/subdir
+    cp(file.path(repdir, subdir), ".", move=TRUE)
+    rmdir(repdir)
+    ## Compress subdir as subdir_sha.tar.gz
+    subtargz <- paste0(subdir, "_", sha, ".tar.gz")
+    tar(subtargz, subdir, compression="gzip")
+    unlink(subdir, recursive=TRUE, force=TRUE)
+  }
 }
