a6d2c2d83012b1d5ca366872a3fd871845698f12
Arni Magnusson <arnima@hafro.is>
2019-08-13 11:12:41 +0200

    Change names: lib -> pkg, etc.

--- R/clean.library.R
+++ R/clean.library.R
@@ -6,22 +6,21 @@
 
 clean.library <- function()
 {
-  lib.entries <- dir("library")
-
   ## For each package, we select between three cases:
   ## 1 Installed package matches SOFTWARE.bib - do nothing
   ## 2 Installed package is not the version listed in SOFTWARE.bib - remove
   ## 3 Installed package is not listed in SOFTWARE.bib - remove
 
-  for(lib in lib.entries)
+  installed <- dir("library")
+  for(pkg in installed)
   {
-    ## Read sha.lib, the SHA for an installed package
-    sha.lib <- packageDescription(lib, lib.loc="library")$RemoteSha
+    ## Read sha.inst, the SHA for an installed package
+    sha.inst <- packageDescription(pkg, lib.loc="library")$RemoteSha
     ## Read sha.bib, the corresponding SHA from SOFTWARE.bib
     bib <- read.bib("SOFTWARE.bib")
-    if(lib %in% names(bib))
+    if(pkg %in% names(bib))
     {
-      repo <- bib[lib]$source
+      repo <- bib[pkg]$source
       spec <- parse_repo_spec(repo)
       sha.bib <- get_remote_sha(spec$username, spec$repo, spec$ref)
       sha.bib <- substring(sha.bib, 1, 7)
@@ -31,8 +30,8 @@ clean.library <- function()
       sha.bib <- "Not listed"
     }
 
-    ## If installed package is either a mismatch or not listed, we remove it
-    if(sha.lib != sha.bib)
-      unlink(file.path("library", lib), recursive=TRUE)
+    ## If installed package is either a mismatch or not listed, then remove it
+    if(sha.inst != sha.bib)
+      unlink(file.path("library", pkg), recursive=TRUE)
   }
 }
