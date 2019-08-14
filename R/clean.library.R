75ef7b8e897655b4ea669cf8e2daad43c69fbf33
Arni Magnusson <arnima@hafro.is>
2019-08-14 14:45:17 +0200

    New function clean.library()

--- R/clean.library.R
+++ R/clean.library.R
@@ -1,24 +1,49 @@
-## Selectively remove packages from bootstrap/library if not in SOFTWARE.bib
-
+#' Clean TAF Library
+#'
+#' Selectively remove packages from the local TAF library if not listed in
+#' \verb{SOFTWARE.bib}.
+#'
+#' @param lib location of local TAF library.
+#'
+#' @note
+#' For each package, the cleaning procedure selects between three cases:
+#' \enumerate{
+#' \item Installed package matches \verb{SOFTWARE.bib} - do nothing.
+#' \item Installed package is not the version listed in \verb{SOFTWARE.bib} -
+#' remove.
+#' \item Installed package is not listed in \verb{SOFTWARE.bib} - remove.
+#' }
+#'
+#' The \code{taf.bootstrap} procedure cleans the TAF library, without requiring
+#' the user to run \code{clean.library}. The main reason for a TAF user to run
+#' \code{clean.library} directly is to experiment with installing and removing
+#' different versions of software without modifying the \verb{SOFTWARE.bib}
+#' file.
+#'
+#' @seealso
+#' \code{\link{taf.bootstrap}} calls \code{clean.library} as a part of the
+#' default bootstrap procedure.
+#'
+#' \code{\link{taf.install}} installs a package in the local TAF library.
+#'
+#' \code{\link{icesTAF-package}} gives an overview of the package.
+#'
 #' @importFrom bibtex read.bib
 #' @importFrom remotes parse_repo_spec
 #' @importFrom utils packageDescription
+#'
+#' @export
 
-clean.library <- function()
+clean.library <- function(lib="bootstrap/library")
 {
-  ## For each package, we select between three cases:
-  ## 1 Installed package matches SOFTWARE.bib - do nothing
-  ## 2 Installed package is not the version listed in SOFTWARE.bib - remove
-  ## 3 Installed package is not listed in SOFTWARE.bib - remove
-
-  installed <- dir("library")
+  installed <- dir(lib)
 
   for(pkg in installed)
   {
     ## Read sha.inst, the SHA for an installed package
-    sha.inst <- packageDescription(pkg, lib.loc="library")$RemoteSha
+    sha.inst <- packageDescription(pkg, lib.loc=lib)$RemoteSha
     ## Read sha.bib, the corresponding SHA from SOFTWARE.bib
-    bib <- read.bib("SOFTWARE.bib")
+    bib <- read.bib(file.path(lib, "../SOFTWARE.bib"))
     if(pkg %in% names(bib))
     {
       repo <- bib[pkg]$source
@@ -33,8 +58,8 @@ clean.library <- function()
 
     ## If installed package is either a mismatch or not listed, then remove it
     if(sha.inst != sha.bib)
-      unlink(file.path("library", pkg), recursive=TRUE)
+      unlink(file.path(lib, pkg), recursive=TRUE)
   }
 
-  rmdir("library")
+  rmdir(lib)
 }
