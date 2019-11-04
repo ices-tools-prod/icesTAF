0d4b293b5c2f63f7ac05191f46a6809890620120
Arni Magnusson <arnima@hafro.is>
2019-11-04 16:11:28 +0100

    Add arg 'seven'

--- R/get_remote_sha.R
+++ R/get_remote_sha.R
@@ -7,7 +7,7 @@
 ## icesTAF:::get.remote.sha("ices-tools-prod", "icesTAF", "3.1-1")
 ## icesTAF:::get.remote.sha("ices-tools-prod", "icesTAF",
 
-get_remote_sha <- function(username, repo, ref)
+get_remote_sha <- function(username, repo, ref, seven=TRUE)
 {
   ## GitHub API URL to get head commit at a reference
   url <- paste("https://api.github.com/repos", username, repo, "commits",
@@ -15,5 +15,9 @@ get_remote_sha <- function(username, repo, ref)
   ## Read and extract SHA code
   sha <- readLines(url, n=2)[2]
   sha <- sub("  \"sha\": \"(.*)\",", "\\1", sha)
+  ## Truncate to seven characters
+  if(seven)
+    sha <- substring(sha, 1, 7)
+
   sha
 }
