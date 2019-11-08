ices-tools-prod/icesTAF

commit 3e35d91389ff2e2d3f74b94e11e8e6d43ec1ae0a

Look up SHA code more efficiently, removing jsonlite dependency

DESCRIPTION
@@ -1,12 +1,12 @@
 Package: icesTAF
 Version: 3.2-0
-Date: 2019-10-02
+Date: 2019-10-30
 Title: Functions to Support the ICES Transparent Assessment Framework
 Authors@R: c(person("Arni", "Magnusson", role=c("aut","cre"), email="arni.magnusson@ices.dk"),
              person("Colin", "Millar", role="aut"),
              person("Alexandros", "Kokkalis", role="ctb"))
 Imports: grDevices, lattice, stats, tools, utils,
-  bibtex, jsonlite, remotes
+  bibtex, remotes
 LazyData: yes
 Description: Functions to support the ICES Transparent Assessment Framework
   <http://taf.ices.dk> to organize data, methods, and results used in ICES

NAMESPACE
@@ -56,7 +56,6 @@ export(zoom)
 export(zoom.trellis)
 importFrom(bibtex,read.bib)
 importFrom(grDevices,png)
-importFrom(jsonlite,read_json)
 importFrom(lattice,xyplot)
 importFrom(remotes,parse_repo_spec)
 importFrom(stats,xtabs)

R/taf.install.R
@@ -102,7 +102,6 @@ already.in.taf.library <- function(targz, lib)
 
 ## Get the SHA code of the remote for a given reference
 
-#' @importFrom jsonlite read_json
 #' @importFrom utils URLencode download.file
 
 ## Internal use examples:
@@ -115,10 +114,8 @@ get_remote_sha <- function(username, repo, ref)
   ## GitHub API URL to get head commit at a reference
   url <- paste("https://api.github.com/repos", username, repo, "commits",
                URLencode(ref, reserved=TRUE), sep="/")
-  ## Download JSON contents to temporary file
-  tmp <- tempfile()
-  on.exit(unlink(tmp), add=TRUE)
-  download.file(url, tmp, quiet=TRUE)
-  res <- read_json(tmp)
-  res$sha
+  ## Read and extract SHA code
+  sha <- readLines(url, n=2)[2]
+  sha <- sub("  \"sha\": \"(.*)\",", "\\1", sha)
+  sha
 }

commit 132e6ba83bc2222b66e6812b60cdd28ec55150f0

Fix bug introduced in 3e35d91

DESCRIPTION
@@ -1,6 +1,6 @@
 Package: icesTAF
 Version: 3.3-0
-Date: 2019-11-05
+Date: 2019-11-08
 Title: Functions to Support the ICES Transparent Assessment Framework
 Authors@R: c(person("Arni", "Magnusson", role=c("aut","cre"), email="arni.magnusson@ices.dk"),
              person("Colin", "Millar", role="aut"),

R/get.remote.sha.R
@@ -30,10 +30,14 @@ get.remote.sha <- function(owner, repo, ref, seven=TRUE)
 {
   ## GitHub API URL to get head commit at a reference
   url <- paste("https://api.github.com/repos", owner, repo, "commits",
-               URLencode(ref,reserved=TRUE), sep="/")
+               URLencode(ref, reserved=TRUE), sep="/")
+
   ## Read and extract SHA code
-  sha <- readLines(url, n=2)[2]
-  sha <- sub("  \"sha\": \"(.*)\",", "\\1", sha)
+  sha <- readLines(url, warn=FALSE)  # one line in Windows, many lines in Linux
+  sha <- paste(sha, collapse="")     # format as one line
+  sha <- gsub(" ", "", sha)          # with no spaces
+  sha <- sub("\\{\"sha\":\"(.*?)\".*", "\\1", sha)  # extract SHA code
+
   ## Truncate to seven characters
   if(seven)
     sha <- substring(sha, 1, 7)