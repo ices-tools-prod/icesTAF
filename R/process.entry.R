66881ef72e92ce5bde057d174739ddc668a8ab76
Arni Magnusson <arni.magnusson@ices.dk>
2020-04-19 11:02:58 +0200

    Only download files that do not already exist

--- R/process.entry.R
+++ R/process.entry.R
@@ -28,7 +28,16 @@ process.entry <- function(bib, dir, quiet)
           grepl("^https://", bib$source[1]) ||
           grepl("^ftp://", bib$source[1]))
   {
-    sapply(bib$source, download, dir=dir, quiet=quiet)
+    destfile <- basename(bib$source)
+    destfile <- gsub("\\?.*", "", destfile)   # file.dat?tail -> file.dat
+    destfile <- gsub(" |%20", "_", destfile)  # my%20script.R -> my_script.R
+    for(i in seq_len(bib$source))
+    {
+      if(!file.exists(file.path(dir, destfile[i])))
+        download(bib$source[i], dir=dir, quiet=quiet)
+      else if(!quiet)
+        message("  Skipping download of '", destfile[i], "' (already in place).")
+    }
   }
 
   ## Case 3: R script in bootstrap directory
