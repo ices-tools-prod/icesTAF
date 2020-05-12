65590deeeadf376f82326cd5e2c8567960e7917a
Arni Magnusson <arni.magnusson@ices.dk>
2020-05-12 18:44:28 +0200

    Add argument 'details' to taf.session()

--- R/taf.session.R
+++ R/taf.session.R
@@ -4,28 +4,22 @@
 #' packages were loaded from the local TAF library.
 #'
 #' @param sort whether to sort packages by name.
+#' @param details whether to include more detailed session information.
 #'
-#' @return
-#' Data frame containing four columns:
-#' \item{Package}{package name}
-#' \item{Version}{package version}
-#' \item{Library}{library where package was loaded from}
-#' \item{RemoteSha}{GitHub reference code}
-#' and \verb{libPath} as an attribute.
+#' @return List containing session information about loaded packages.
 #'
 #' @seealso
-#' \code{\link{sessionInfo}} and the \pkg{sessioninfo} package provide further
-#' session information about the version of R, etc.
-#'
-#' \code{\link{taf.libPaths}} adds the TAF library to the search path for R
-#' packages.
+#' \code{\link{sessionInfo}} and the \pkg{sessioninfo} package provide similar
+#' information, but do not indicate clearly packages that were loaded from the
+#' local TAF library.
 #'
 #' \code{\link{icesTAF-package}} gives an overview of the package.
 #'
 #' @examples
 #' \donttest{
 #' taf.session()
-#' taf.session(TRUE)
+#' taf.session(sort=TRUE)
+#' taf.session(details=TRUE)
 #' }
 #'
 #' @importFrom utils sessionInfo
@@ -33,38 +27,48 @@
 #'
 #' @export
 
-taf.session <- function(sort=FALSE)
+taf.session <- function(sort=FALSE, details=FALSE)
 {
   info <- function(desc)
   {
     lib <- dirname(find.package(desc$Package))
-    desc$Library <- if(basename(dirname(lib)) == "bootstrap")
-                      "TAF" else paste0("[", match(lib, .libPaths()), "]")
+    desc$Library <- if(lib %in% .libPaths())
+                      paste0("[", match(lib, .libPaths()), "]") else "TAF"
     desc$RemoteSha <- if(is.null(desc$RemoteSha))
                         "" else substring(desc$RemoteSha, 1, 7)
-    fields <- c("Package", "Version", "Library", "RemoteSha")
+    desc$TAF <- if(basename(dirname(lib)) == "bootstrap") "*" else ""
+    fields <- c("Package", "Version", "Library", "RemoteSha", "TAF")
     if(identical(desc$Priority, "base"))
-      setNames(rep(NA_character_, 4), fields)
+      setNames(rep(NA_character_, 5), fields)
     else
       unlist(desc[fields])
   }
 
   si <- sessionInfo()
 
-  pkgs <- sapply(c(si$otherPkgs, si$loadedOnly), info)
-  pkgs <- pkgs[,apply(pkgs, 2, function(x) !all(is.na(x)))]
-  pkgs <- data.frame(t(pkgs), row.names=NULL)
+  version <- R.version$version.string
+  date <- Sys.Date()
+  packages <- sapply(c(si$otherPkgs, si$loadedOnly), info)
+  packages <- packages[,apply(packages, 2, function(x) !all(is.na(x)))]
+  packages <- data.frame(t(packages), row.names=NULL)
   if(sort)
-    pkgs <- data.frame(pkgs[order(pkgs$Package),], row.names=NULL)
+    packages <- data.frame(packages[order(packages$Package),], row.names=NULL)
+  libPaths <- data.frame(Entry=.libPaths())
+  out <- list(version=version, date=date, packages=packages, libPaths=libPaths)
 
-  key <- paste0("[", seq_along(.libPaths()), "] ", .libPaths())
-  if(basename(dirname(.libPaths()[1])) == "bootstrap")
-    key <- sub("^\\[1\\]", "TAF", key)
-  key <- paste0("\n", paste(key, collapse="\n"), "\n")
+  if(details)
+  {
+    running <- sessionInfo()$running
+    arch <- R.version$arch
+    user <- Sys.info()[["user"]]
+    encoding <- unlist(l10n_info())
+    locale <- unlist(strsplit(Sys.getlocale(), ";"))
+    locale <- strsplit(locale, "=")
+    locale <- data.frame(matrix(unlist(locale), ncol=2, byrow=TRUE,
+                                dimnames=list(NULL,c("Variable","Value"))))
+    out <- c(out, list(running=running, arch=arch, user=user,
+                       encoding=encoding, locale=locale))
+  }
 
-  print(pkgs)
-  cat(key)
-
-  attr(pkgs, "libPaths") <- .libPaths()
-  invisible(pkgs)
+  out
 }
