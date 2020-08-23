d6c99d6ce3a0fb7a1b098df3c52d28ada2d46e37
Colin Millar <colin.millar@ices.dk>
2020-08-23 12:33:01 +0200

    * add function taf.sources() to extract data and software sources used in a project

---
+++ R/taf.sources.R
@@ -0,0 +1,32 @@
+#' Extract sources from TAF *.bib file
+#'
+#' Extract a list of sources from a TAF *.bib file (i.e. DATA.bib or
+#' SOFTWARE.bib). This allows the user to print the lisst of sources
+#' but also to process them individualy, giving more flexibiulity when
+#' developing larger projects.
+#'
+#' @param type one of "data", "software" or "both"
+#'
+#' @seealso
+#' \link{process.entry} to process one of the entries returned by
+#' `taf.sources`.
+#'
+#' @export
+
+taf.sources <- function(type) {
+  # check type arg
+  type = match.arg(type, c("data", "software", "both"))
+
+  bibfile <- file.path("bootstrap", paste0(toupper(type), ".bib"))
+  sources <- bibtex::read.bib(bibfile)
+  sources <-
+    lapply(
+      sources,
+      function(x) {
+        x$type = type
+        x
+      }
+    )
+  names(sources) <- NULL
+  do.call("c", sources)
+}
